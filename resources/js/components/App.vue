c22dcc9574e65921368db93827849f82cef848db
unknown <iwf18003@icewingfilm.com>
2019-08-22 10:37:37 +0800

    test

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,21 +1,12 @@
 <template>
     <div id="app">
-<<<<<<< HEAD
         <navbar></navbar>
         
-=======
-        <h1> Hello App! </h1>
-        <p>
-            <router-link to='/foo'>Go To Foo</router-link>
-            <router-link to='/bar'>Go To Bar</router-link>
-        </p>
->>>>>>> 133b84586c5a6380aacfec157b516ce82eeaa890
         <router-view></router-view>
     </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import Navbar from './Navbar.vue';
 
 export default {
@@ -27,22 +18,6 @@ export default {
     },
     methods: {
 
-=======
-
-export default {
-    components: {
-
-    },
-    computed: {
-        username() {
-            return this.$route.params.username
-        }
-    },
-    methods: {
-        goBack() {
-            window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
-        }
->>>>>>> 133b84586c5a6380aacfec157b516ce82eeaa890
     }
 }
 </script>
